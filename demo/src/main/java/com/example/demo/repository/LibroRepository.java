354aa6eb0fb0d5c1232288bd764728020a37a088
CarlosPV-oss <carlospv18@educastur.es>
2025-10-27 20:16:28 +0100

    service

---
+++ demo/src/main/java/com/example/demo/repository/LibroRepository.java
@@ -0,0 +1,17 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Libro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LibroRepository extends JpaRepository<Libro,Long> {
+
+  /*  @Query("SELECT l FROM Libro l")
+
+    List<Libro> listaLibros();*/
+
+}
---
+++ demo/src/main/java/com/example/demo/service/LibroService.java
@@ -0,0 +1,14 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Libro;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface LibroService {
+
+    List<Libro> getLibros();
+    Optional<Libro> getLibro(Long id);
+    void guardarOActualizarLibro(Libro Libro);
+    void eliminarLibro(Long id);
+}
